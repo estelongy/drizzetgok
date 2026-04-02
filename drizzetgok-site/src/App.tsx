6943cb2a44b64640ea4118828d423614bc184cd3
estelongy <estelongy@gmail.com>
2026-04-02 13:37:23 +0300

    Update App.tsx

--- drizzetgok-site/src/App.tsx
+++ drizzetgok-site/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Bio from './pages/Bio';
@@ -7,8 +7,11 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Ana sayfa linki hem boş hem de #anasayfa için çalışsın */}
         <Route path="/" element={<Home />} />
+        <Route path="/anasayfa" element={<Home />} />
         <Route path="/bio" element={<Bio />} />
+        {/* Eğer başka özel sayfaların varsa buraya ekleyeceğiz */}
       </Routes>
     </Router>
   );
